2b059e5ff27ca274751bcfccf58aa315ecbc060c
t4upl <tomr33d32@gmail.com>
2020-11-21 12:51:08 +0100

    TestUtils | parseListofLists

--- src/test/java/com/artifactId/leetcode/TestUtils.java
+++ src/test/java/com/artifactId/leetcode/TestUtils.java
@@ -35,4 +35,25 @@ public class TestUtils {
     return treeNodes.get(0);
   }
 
+  public static List<List<Integer>> parseListOfLists(String s) {
+    s = s.substring(1, s.length());
+    List<List<Integer>> result = new ArrayList<>();
+    List<Integer> acc = new ArrayList<>();
+    s = s.replace("[","");
+    String[] split = s.split("]");
+    for (String s1 : split) {
+      String[] numbers = s1.split(",");
+      acc = new ArrayList<>();
+      for (String number : numbers) {
+        if (number.isEmpty()) {
+          continue;
+        }
+
+        acc.add(Integer.parseInt(number));
+      }
+      result.add(acc);
+    }
+
+    return result;
+  }
 }
