461c7293c7ddd85ed7f03fc1783bf1eece78b948
t4upl <tomr33d32@gmail.com>
2020-11-23 20:08:35 +0100

    Leetcode medium | House Robber III

---
+++ src/main/java/com/artifactId/leetcode/medium/houserobberthree/Solution.java
@@ -0,0 +1,38 @@
+package com.artifactId.leetcode.medium.houserobberthree;
+
+import com.artifactId.leetcode.TreeNode;
+
+/**
+ * Runtime: 1134 ms, faster than 5.02% of Java online submissions for House Robber III.
+ * Memory Usage: 38.6 MB, less than 64.51% of Java online submissions for House Robber III.
+ */
+public class Solution {
+  public int rob(TreeNode root) {
+    return dfs(root);
+  }
+
+  private int dfs(TreeNode treeNode) {
+    if (treeNode == null) {
+      return 0;
+    }
+
+    TreeNode l1 = null;
+    TreeNode l2 = null;
+    TreeNode r1 = null;
+    TreeNode r2 = null;
+    if (treeNode.left != null) {
+      l1 = treeNode.left.left;
+      l2 = treeNode.left.right;
+    }
+
+    if (treeNode.right!= null) {
+      r1 = treeNode.right.left;
+      r2 = treeNode.right.right;
+    }
+
+    int i = treeNode.val + dfs(l1) + dfs(l2) + dfs(r1) + dfs(r2);
+    int j = dfs(treeNode.left) + dfs(treeNode.right);
+    return Math.max(i, j);
+  }
+
+}
---
+++ src/test/java/com/artifactId/leetcode/medium/houserobberthree/SolutionTest.java
@@ -0,0 +1,36 @@
+package com.artifactId.leetcode.medium.houserobberthree;
+
+import com.artifactId.leetcode.TestUtils;
+import com.artifactId.leetcode.TreeNode;
+import org.junit.Before;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SolutionTest {
+
+  private Solution solution;
+
+  @Before
+  public void setUp() throws Exception {
+    solution = new Solution();
+  }
+
+  @Test
+  public void leet1() {
+    TreeNode root = TestUtils.parseTreeNode("[3,2,3,null,3,null,1]");
+    int expected = 7;
+    int result = solution.rob(root);
+    Assert.assertEquals(expected, result);
+  }
+
+  @Test
+  public void leet2() {
+    TreeNode root = TestUtils.parseTreeNode("[3,4,5,1,3,null,1]");
+    int expected = 9;
+    int result = solution.rob(root);
+    Assert.assertEquals(expected, result);
+  }
+
+
+}
