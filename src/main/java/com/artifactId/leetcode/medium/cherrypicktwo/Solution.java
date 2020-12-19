e1893b142fb0c1789fac84fecddb759eb118ef15
t4upl <tomr33d32@gmail.com>
2020-12-19 18:08:24 +0100

    Leetcode hard | Cherry Pickup II

---
+++ src/main/java/com/artifactId/leetcode/medium/cherrypicktwo/Solution.java
@@ -0,0 +1,99 @@
+package com.artifactId.leetcode.medium.cherrypicktwo;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * Runtime: 138 ms, faster than 5.92% of Java online submissions for Cherry Pickup II.
+ * Memory Usage: 47 MB, less than 8.80% of Java online submissions for Cherry Pickup II.
+ */
+public class Solution {
+
+  private int columns;
+  private int rows;
+
+  private final Map<Entry, Integer> memoization = new HashMap<>();
+
+  public int cherryPickup(int[][] grid) {
+    if (grid.length == 0) {
+      return 0;
+    }
+
+    this.rows = grid.length;
+    this.columns = grid[0].length;
+
+
+    return dfs(0, columns - 1, 0, grid);
+  }
+
+  private int dfs(int robotOnePosition, int robotTwoPosition, int row, int[][] grid) {
+    Entry memoizationKey = new Entry(robotOnePosition, robotTwoPosition, row);
+    Integer memoizationVal = memoization.get(memoizationKey);
+    if (memoizationVal != null) {
+      return memoizationVal;
+    }
+
+    int sum = grid[row][robotOnePosition] + grid[row][robotTwoPosition];
+    if (robotOnePosition == robotTwoPosition) {
+      sum-= grid[row][robotOnePosition];
+    }
+
+    int lowerRowsSum = 0;
+
+    row++;
+    if (row < this.rows) {
+      int newRobotOnePosition;
+      int newRobotTwoPosition;
+      for (int i = -1; i < 2; i++) {
+        for (int j = -1; j < 2; j++) {
+          newRobotOnePosition = robotOnePosition + i;
+          newRobotTwoPosition = robotTwoPosition + j;
+          if (isPositionLegal(newRobotOnePosition) && isPositionLegal(newRobotTwoPosition)) {
+            lowerRowsSum = Math.max(dfs(newRobotOnePosition, newRobotTwoPosition, row, grid), lowerRowsSum);
+          }
+        }
+      }
+    }
+
+    int answer = sum + lowerRowsSum;
+    memoization.put(memoizationKey, answer);
+    return answer;
+  }
+
+  private boolean isPositionLegal(int position) {
+    return position > -1 && position < this.columns;
+  }
+
+  private class Entry {
+    private int robotOnePosition;
+    private int robotTwoPosition;
+    private int row;
+
+    public Entry(int robotOnePosition, int robotTwoPosition, int row) {
+      this.robotOnePosition = robotOnePosition;
+      this.robotTwoPosition = robotTwoPosition;
+      this.row = row;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) {
+        return true;
+      }
+      if (o == null || getClass() != o.getClass()) {
+        return false;
+      }
+      Entry entry = (Entry) o;
+      return robotOnePosition == entry.robotOnePosition &&
+        robotTwoPosition == entry.robotTwoPosition &&
+        row == entry.row;
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(robotOnePosition, robotTwoPosition, row);
+    }
+  }
+
+}
---
+++ src/test/java/com/artifactId/leetcode/medium/cherrypicktwo/SolutionTest.java
@@ -0,0 +1,71 @@
+package com.artifactId.leetcode.medium.cherrypicktwo;
+
+import org.junit.Before;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SolutionTest {
+
+  private Solution solution;
+
+  @Before
+  public void setUp() throws Exception {
+    solution = new Solution();
+  }
+
+  @Test
+  public void leet1() {
+    int[][] grid = {{3,1,1}, {2,5,1}, {1,5,5}, {2,1,1}};
+    int expected = 24;
+    int result = solution.cherryPickup(grid);
+    Assert.assertEquals(expected, result);
+  }
+
+  @Test
+  public void leet2() {
+    int[][] grid = {{1,0,0,0,0,0,1},{2,0,0,0,0,3,0},{2,0,9,0,0,0,0},{0,3,0,5,4,0,0},{1,0,2,3,0,0,6}};
+    int expected = 28;
+    int result = solution.cherryPickup(grid);
+    Assert.assertEquals(expected, result);
+  }
+
+  @Test
+  public void leet3() {
+    int[][] grid = {{1,0,0,3},{0,0,0,3},{0,0,3,3},{9,0,3,3}};
+    int expected = 22;
+    int result = solution.cherryPickup(grid);
+    Assert.assertEquals(expected, result);
+  }
+
+
+  @Test
+  public void leet4() {
+    int[][] grid = {{1,1},{1,1}};
+    int expected = 4;
+    int result = solution.cherryPickup(grid);
+    Assert.assertEquals(expected, result);
+  }
+
+  @Test
+  public void tets1() {
+    int[][] grid = {{10}};
+    int expected = 10;
+    int result = solution.cherryPickup(grid);
+    Assert.assertEquals(expected, result);
+  }
+
+  @Test
+  public void tets2() {
+    int[][] grid = {{10, 1, 20}, {1, 30, 40}, {50, 1, 60}, {90, 80, 1}};
+    int expected = 380;
+    int result = solution.cherryPickup(grid);
+    Assert.assertEquals(expected, result);
+  }
+
+
+
+
+
+
+}
