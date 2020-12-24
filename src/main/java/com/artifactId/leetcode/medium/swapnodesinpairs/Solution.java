af012dbc84246bfc1e3752f0628f0293a92e1605
t4upl <tomr33d32@gmail.com>
2020-12-24 23:48:24 +0100

    Leetcode medium | Swap Nodes in Pairs

---
+++ src/main/java/com/artifactId/leetcode/medium/swapnodesinpairs/Solution.java
@@ -0,0 +1,51 @@
+package com.artifactId.leetcode.medium.swapnodesinpairs;
+
+import com.artifactId.leetcode.other.reversenodesinkgroup.ListNode;
+
+/**
+ * Runtime: 0 ms, faster than 100.00% of Java online submissions for Swap Nodes in Pairs.
+ * Memory Usage: 36.8 MB, less than 21.05% of Java online submissions for Swap Nodes in Pairs.
+ */
+public class Solution {
+
+  public ListNode swapPairs(ListNode head) {
+    if (head == null) {
+      return null;
+    }
+
+    ListNode firstNode = head;
+    ListNode secondNode;
+    ListNode newFirstNode;
+    ListNode tail = head;
+    ListNode ans = null;
+
+    while (firstNode != null && firstNode.next != null) {
+      secondNode = firstNode.next;
+      if (ans == null) {
+        ans = secondNode;
+      }
+
+      newFirstNode = secondNode.next;
+      secondNode.next = firstNode;
+      tail.next = secondNode;
+
+      tail = firstNode;
+      firstNode = newFirstNode;
+    }
+
+    if (firstNode != null) {
+      tail.next = firstNode;
+      tail = firstNode;
+
+    }
+
+    tail.next = null;
+
+    if (ans == null) {
+      ans = head;
+    }
+
+    return ans;
+  }
+
+}
---
+++ src/test/java/com/artifactId/leetcode/medium/swapnodesinpairs/SolutionTest.java
@@ -0,0 +1,98 @@
+package com.artifactId.leetcode.medium.swapnodesinpairs;
+
+import com.artifactId.leetcode.other.reversenodesinkgroup.ListNode;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SolutionTest {
+
+  private Solution solution;
+
+  @Before
+  public void setUp() throws Exception {
+    solution = new Solution();
+  }
+
+  @Test
+  public void test1() {
+    ListNode listNode = new ListNode(1);
+    List<Integer> expected= Arrays.asList(1);
+    ListNode result = solution.swapPairs(listNode);
+    Assert.assertEquals(expected, toList(result));
+  }
+
+  @Test
+  public void test2() {
+    ListNode listNodeOne = new ListNode(1);
+    ListNode listNodeTwo = new ListNode(2);
+    listNodeOne.next = listNodeTwo;
+
+
+    List<Integer> expected= Arrays.asList(2, 1);
+    ListNode result = solution.swapPairs(listNodeOne);
+    Assert.assertEquals(expected, toList(result));
+  }
+
+  @Test
+  public void test3() {
+    ListNode listNodeOne = new ListNode(1);
+    ListNode listNodeTwo = new ListNode(2);
+    ListNode listNodeThree = new ListNode(3);
+    ListNode listNodeFour = new ListNode(4);
+    listNodeOne.next = listNodeTwo;
+    listNodeTwo.next = listNodeThree;
+    listNodeThree.next = listNodeFour;
+
+
+    List<Integer> expected= Arrays.asList(2, 1, 4, 3);
+    ListNode result = solution.swapPairs(listNodeOne);
+    Assert.assertEquals(expected, toList(result));
+  }
+
+  @Test
+  public void test4() {
+    ListNode listNodeOne = new ListNode(1);
+    ListNode listNodeTwo = new ListNode(2);
+    ListNode listNodeThree = new ListNode(3);
+    listNodeOne.next = listNodeTwo;
+    listNodeTwo.next = listNodeThree;
+
+    List<Integer> expected= Arrays.asList(2, 1, 3);
+    ListNode result = solution.swapPairs(listNodeOne);
+    Assert.assertEquals(expected, toList(result));
+  }
+
+  @Test
+  public void test5() {
+    ListNode listNodeOne = new ListNode(1);
+    ListNode listNodeTwo = new ListNode(2);
+    ListNode listNodeThree = new ListNode(3);
+    ListNode listNodeFour = new ListNode(4);
+    ListNode listNodeFive = new ListNode(5);
+    listNodeOne.next = listNodeTwo;
+    listNodeTwo.next = listNodeThree;
+    listNodeThree.next = listNodeFour;
+    listNodeFour.next = listNodeFive;
+
+
+    List<Integer> expected= Arrays.asList(2, 1, 4, 3, 5);
+    ListNode result = solution.swapPairs(listNodeOne);
+    Assert.assertEquals(expected, toList(result));
+  }
+
+  private List<Integer> toList(ListNode result) {
+    List<Integer> list = new ArrayList<>();
+    while (result != null) {
+      list.add(result.getVal());
+      result = result.next;
+    }
+    return list;
+  }
+
+
+}
